a22f8c6515ad8aa1cde9e3e4447d4150259001b5
truongquangdang97 <70947896+truongquangdang97@users.noreply.github.com>
2021-06-30 10:34:42 +0700

    update

---
+++ resources/views/HomeProduct/ShowProduct.blade.php
@@ -0,0 +1,43 @@
+@extends("layout")
+@section("main")
+    <div class="content-wrapper">
+        <div class="container">
+            <div class="row">
+                <div class="col-md-12" style="text-align: center;margin-top: 20px">
+                    <h1>Danh sách các sản phẩm của công ty </h1>
+                </div>
+                <div class="container">
+                    <div class="row">
+                        <div class="col-md-4">
+                            <div style="border: 1px solid #b0b0b0;padding: 10px">
+                                <h4>Chuối</h4>
+                                <img src="" alt="">
+                                <div>Hạn sử dụng : 4 Ngày kể từ khi hái </div>
+                                <div>Giá : 10.000 VND</div>
+                                <div></div>
+                                <div style="display: flex;justify-content: space-around">
+                                    <a href="" class="btn btn-outline-danger">Xem chi tiết </a>
+                                    <a href="" class="btn btn-outline-primary">Add to cart </a>
+                                </div>
+
+                            </div>
+                        </div>
+                        <div class="col-md-4 border">
+                            <div>
+                                <h5>Day laf sap q</h5>
+                            </div>
+                        </div>
+                        <div class="col-md-4 border">
+                            <div>
+                                <h5>Day laf sap q</h5>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+@endsection
+<style>
+
+</style>
--- routes/web.php
+++ routes/web.php
@@ -2,7 +2,7 @@
 
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\departmentsController;
-
+use App\Http\Controllers\ProductController;
 /*
 |--------------------------------------------------------------------------
 | Web Routes
@@ -22,3 +22,8 @@ Route::post('/departments/save',[departmentsController::class ,"save"]);
 Route::get('/departments/delete/{id}',[departmentsController::class ,"delete"]);
 Route::get('/departments/edit/{id}',[departmentsController::class ,"edit"]);
 Route::post('/departments/update/{id}',[departmentsController::class ,"update"]);
+
+Route::get('/product/show',[ProductController::class ,"show"]);
+
+
+
